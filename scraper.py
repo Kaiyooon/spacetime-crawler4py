5c5e6e18c48d6d635d4445d34ac1ea8013b708c3
Kei Asakawa <77090883+Kaiyooon@users.noreply.github.com>
2022-04-15 11:46:41 -0700

    Implemented BeautifulSoup for filtering

--- scraper.py
+++ scraper.py
@@ -1,8 +1,3 @@
-# Kei's Attempt :)
-# Jake's attempt to edit the file.
-# Lyla's change
-# change #2
-#
 '''
 ================================================================================
 TO-DO:
@@ -62,6 +57,7 @@ QUESTIONS FOR TA:
 
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -77,7 +73,15 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    hyperlinks = list()
+    if resp.status == 200:
+        # Use BeautifulSoup to filter links from content
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        for link in soup.find_all('a'):
+            hyperlinks.append(link.get('href'))
+        print(hyperlinks)
+    hyperlinks = list()
+    return hyperlinks
 
 def is_valid(url):
     # Decide whether to crawl this url or not.
