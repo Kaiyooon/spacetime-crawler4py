db485e291f16a4f95c9051f00c5f8e35ffaeca22
Jake Hudelson <jake@jakehudelson.com>
2022-04-28 19:10:09 -0700

    Update worker.py
    Added a try block in worker.py

--- crawler/worker.py
+++ crawler/worker.py
@@ -15,7 +15,7 @@ class Worker(Thread):
         # basic check for requests in scraper
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests from scraper.py"
         super().__init__(daemon=True)
-        
+
     def run(self):
         while True:
             tbd_url = self.frontier.get_tbd_url()
@@ -26,8 +26,13 @@ class Worker(Thread):
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
-            for scraped_url in scraped_urls:
-                self.frontier.add_url(scraped_url)
+            try:
+                scraped_urls = scraper.scraper(tbd_url, resp)
+                for scraped_url in scraped_urls:
+                    self.frontier.add_url(scraped_url)
+            # In a program-crippling error resulting from scraping a URL, it is
+            # in the best interest of the program to continue without that URL's URLs.
+            except Exception:
+                uselessString = ""
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
